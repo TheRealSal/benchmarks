060788e7ed3e029527c1c4c7dc7cdf3460baca59
TheRealSal <75696327+TheRealSal@users.noreply.github.com>
2025-04-29 01:27:44 -0400

    Adjusted FFT code to work with WandB

--- benchmarks/CL_MASR/whisper/train_ft.py
+++ benchmarks/CL_MASR/whisper/train_ft.py
@@ -287,10 +287,10 @@ def test(hparams, run_opts, locales, wer_file="wer_test.txt"):
         asr_brain.hparams.wer_file = os.path.join(locale_folder, wer_file)
         if hparams["skip_test"]:
             # Dummy test
-            train_log_backup = asr_brain.hparams.train_logger.save_file
-            asr_brain.hparams.train_logger.save_file = (
-                asr_brain.hparams.wer_file
-            ) = os.path.join(locale_folder, "tmp.txt")
+            # train_log_backup = asr_brain.hparams.train_logger.save_file
+            # asr_brain.hparams.train_logger.save_file = (
+            #     asr_brain.hparams.wer_file
+            # ) = os.path.join(locale_folder, "tmp.txt")
             test_data.data_ids = list(test_data.data.keys())[:1]
             test_data.data = {k: test_data.data[k] for k in test_data.data_ids}
             asr_brain.evaluate(
@@ -299,7 +299,7 @@ def test(hparams, run_opts, locales, wer_file="wer_test.txt"):
                 test_loader_kwargs=hparams["valid_dataloader_kwargs"],
             )
             os.remove(asr_brain.hparams.wer_file)
-            asr_brain.hparams.train_logger.save_file = train_log_backup
+            # asr_brain.hparams.train_logger.save_file = train_log_backup
             asr_brain.hparams.wer_file = os.path.join(locale_folder, wer_file)
         else:
             asr_brain.evaluate(
@@ -482,12 +482,12 @@ if __name__ == "__main__":
 
     with open(hparams_file) as fin:
         hparams = load_hyperpyyaml(fin, overrides)
-    hparams["train_logger"].save_file = hparams[
-        "train_logger"
-    ].save_file.replace(
-        ".txt",
-        f"_base={','.join(hparams['base_locales'])}_new={','.join(hparams['new_locales'])}.txt",
-    )
+    # hparams["train_logger"].save_file = hparams[
+    #     "train_logger"
+    # ].save_file.replace(
+    #     ".txt",
+    #     f"_base={','.join(hparams['base_locales'])}_new={','.join(hparams['new_locales'])}.txt",
+    # )
 
     # Create experiment directory
     sb.create_experiment_directory(
