ab752e916a3e05383c4d74b4cafe5813e39926f9
TheRealSal <75696327+TheRealSal@users.noreply.github.com>
2025-09-10 14:50:58 -0400

    Use only one half H100

--- benchmarks/CL_MASR/whisper/scripts/AB/Scaling/Encoder/AB_Bn_Scaling.sh
+++ benchmarks/CL_MASR/whisper/scripts/AB/Scaling/Encoder/AB_Bn_Scaling.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=16G
 #SBATCH --account=def-ravanelm
-#SBATCH --gpus=nvidia_h100_80gb_hbm3_3g.40gb:2
+#SBATCH --gpus=nvidia_h100_80gb_hbm3_3g.40gb:1
 #SBATCH --nodes=1
 #SBATCH --output=logs/AB/Scaling/array_%A_%a.out
 
--- benchmarks/CL_MASR/whisper/scripts/EO/Enc_Dec/EO_Bn.sh
+++ benchmarks/CL_MASR/whisper/scripts/EO/Enc_Dec/EO_Bn.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=16G
 #SBATCH --account=def-ravanelm
-#SBATCH --gpus=nvidia_h100_80gb_hbm3_3g.40gb:2
+#SBATCH --gpus=nvidia_h100_80gb_hbm3_3g.40gb:1
 #SBATCH --nodes=1
 
 # Load Dataset
